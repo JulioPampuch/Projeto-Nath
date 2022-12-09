8e716fc851baf4d4bc9afa7457e1c85ee175d7f4
Julio Pampuch <juliopampuch@gmail.com>
2022-12-08 23:51:18 -0300

    created header

---
+++ src/Components/Header/header.js
@@ -0,0 +1,11 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+    <section className="mainDiv">
+      <img src="https://as1.ftcdn.net/v2/jpg/01/16/24/44/1000_F_116244459_pywR1e0T3H7FPk3LTMjG6jsL3UchDpht.jpg" />
+    </section>
+  )
+}
+
+export default Header
---
+++ src/Components/Main/main.js
@@ -0,0 +1,12 @@
+import { WhatsappLogo } from "phosphor-react"
+import "./main.css"
+
+const Main = () => {
+  return (
+    <>
+    <WhatsappLogo />
+    </>
+  )
+}
+
+export default Main
